351a5933974c39c7998e415a8011b27f869b84f7
ak-hariharan <ak.hari.coder@gmail.com>
2024-09-10 12:31:43 +0530

    modification in js

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 const matrix = document.querySelector(".matrix");
+const startValue = document.querySelector(".start-value");
+const endValue = document.querySelector(".end-value");
+var selectedElement = document.querySelector(".selected-element");
 
-for (let i = 1; i < 10; i++) {
-  let newSpan = document.createElement("span");
-  newSpan.innerHTML = i * 5;
-  newSpan.classList.add("matrix-element"); 
-  matrix.appendChild(newSpan);
+
+function renderMatrix() {
+  matrix.innerHTML = "";
+    for (let i = startValue.value; i < endValue.value; i++) {
+    matrix.insertAdjacentHTML("beforeend", `<span class="matrix-element">${i * 5}</span>`);
+  }
 }
 
-const selectedElement = document.querySelector(".selected-element");
+startValue.addEventListener("input", renderMatrix);
+endValue.addEventListener("input", renderMatrix);
 
-document.querySelectorAll(".matrix-element").forEach((span) => {
-  span.addEventListener("click", () => {
-    document
-      .querySelectorAll(".matrix-element")
-      .forEach((el) => (el.style.backgroundColor = ""));
-    //el is a parameter representing the current element in the iteration
-    //(el) => (el.style.backgroundColor = "") is an arrow function
-    span.style.backgroundColor = "#1cc6ac";
-    const selectedValue = span.textContent;
-    selectedElement.innerHTML = `Selected value is ${selectedValue}`;
-  });
-});
+renderMatrix();
+
+matrix.addEventListener("click", handleClickEvent);
+var previousElement = null;
+
+function handleClickEvent(event) {
+  let targetElement = event.target;
+  if (previousElement) {
+    previousElement.style.backgroundColor = "";
+  }
+  selectedElement.textContent = `Selected value is ${targetElement.textContent}`;
+  targetElement.style.backgroundColor = "#1cc6ac";
+  previousElement = targetElement;
+  
+}
+
